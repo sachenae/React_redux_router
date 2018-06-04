f6ecabbec28a95287d6c13728e6f9532cc947863
sachins <39sachen@gmail.com>
2018-06-04 14:45:46 +0300

    added functionality for deleting the post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
 export const FETCH_SELECTED_POST = 'fetch_selected_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=k1234';
@@ -32,5 +33,13 @@ export function fetchSelectedPost(id){
     return {
         type: FETCH_SELECTED_POST,
         payload: request
+    }
+}
+
+export function deletePost(id, callback){
+    const request= axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`).then(()=>callback());
+    return {
+        type: DELETE_POST,
+        payload: id
     }
 }
--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchSelectedPost } from '../actions';
+import { fetchSelectedPost, deletePost } from '../actions';
 import { Link } from 'react-router-dom';
+
 
 class PostShow extends Component {
 
     componentDidMount() {
         const { id } = this.props.match.params;
         this.props.fetchSelectedPost(id);
+    }
+
+    OnDeleteClick(){
+        const {id } = this.props.match.params;
+        this.props.deletePost(id, ()=> {
+            this.props.history.push('/');
+        });
     }
     render(){
         const { post } = this.props;
@@ -18,6 +26,12 @@ class PostShow extends Component {
         return(
             <div>
                 <Link to="/">Back To Index</Link>
+                <button 
+                className="btn btn-danger pull-xs-right"
+                onClick={this.OnDeleteClick.bind(this)}
+                >
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h5>Categories: {post.categories}</h5>
                 <p>{post.content}</p>
@@ -30,4 +44,4 @@ function mapStateToProps({ posts }, ownProps ){
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchSelectedPost })(PostShow);
+export default connect(mapStateToProps, { fetchSelectedPost, deletePost })(PostShow);
